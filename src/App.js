b2af5aa37b67b77c84cfd9830d4aca12f8deeb35
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-09-21 13:45:05 -0500

    Created our first functional react component...
    And overused it in App.js...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './Greeting';
 
 function App() {
   return (
@@ -8,6 +9,12 @@ function App() {
       <h1>
         Hello ReactJS ! ! !
       </h1>
+      <Greeting />
+      <Greeting />
+      <Greeting />
+      <Greeting />
+      <Greeting />
+      <Greeting />
       <p>
         Wow, JSX is neat ! ! !
       </p>
---
+++ src/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Functional React component... Guess what, it is a function ! ! !
+const Greeting = () => {
+    return (
+        <h1>
+            Hello again ReactJS ! ! !
+        </h1>
+    );
+}
+
+export default Greeting;
